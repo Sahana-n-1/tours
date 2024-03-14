e1b6ea8a1ce42e70bfc6d37b939d1aeda1e57a42
Sahana-n-1 <sahana20021211@gmail.com>
2024-03-14 10:52:54 +0530

    my first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Cardcontainer from './components/Cardcontainer';
+import Header from "./components/Header";
+import Home from "./components/Home";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  return (
+    <>
+    <div>
+      
+      <Header />
+      <Home />
+      <ToastContainer position='top-right'/>
+          </div>
+    </>
+
+    
+  );
+}
+
+
+export default App;
